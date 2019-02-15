f58437f36de839a7714fe73a392285fec58d1952
xray <creator@darkmattercreations.com>
2019-02-15 17:19:38 -0600

    Added Test: HandleRequestThrowsNotImplementedErrorIfItRecievesEightThousandBytesBeforeItReceivesTwoConsecutiveCRLFs

--- tests/ProtocolHandler/HTTP/ChorizoHTTPConnectionHandlerTest.cs
+++ tests/ProtocolHandler/HTTP/ChorizoHTTPConnectionHandlerTest.cs
@@ -13,20 +13,24 @@ namespace Chorizo.Tests.ProtocolHandler.HTTP
 {
     public class ChorizoHttpConnectionHandlerTest
     {
+        private string _testGetRequestString;
+        private byte[] _testGetRequestBytes;
+
+        public ChorizoHttpConnectionHandlerTest()
+        {
+            _testGetRequestString = "GET / HTTP/1.1\r\n" +
+                                    "cache-control: no-cache\r\n" +
+                                    "Postman-Token: e6ff99e3-02c5-4c16-a4e7-0fb1896333c6\r\n" +
+                                    "User-Agent: PostmanRuntime/7.4.0\r\n" +
+                                    "Accept: */*\r\n" +
+                                    "Host: localhost:5000\r\n" +
+                                    "accept-encoding: gzip, deflate\r\n" +
+                                    "\r\n";
+            _testGetRequestBytes = Encoding.UTF8.GetBytes(_testGetRequestString);
+        }
         [Fact]
         public void HandleRequestHandlesAnyProperlyFormattedHttpRequest()
         {
-            const string testRequestString = "GET / HTTP/1.1\r\n" +
-                                             "cache-control: no-cache\r\n" +
-                                             "Postman-Token: e6ff99e3-02c5-4c16-a4e7-0fb1896333c6\r\n" +
-                                             "User-Agent: PostmanRuntime/7.4.0\r\n" +
-                                             "Accept: */*\r\n" +
-                                             "Host: localhost:5000\r\n" +
-                                             "accept-encoding: gzip, deflate\r\n" +
-                                             "\r\n";
-
-            var testRequestBytes = Encoding.UTF8.GetBytes(testRequestString);
-
             var testParsedData = new ParsedRequestData
             {
                 Method = "GET",
@@ -48,7 +52,7 @@ namespace Chorizo.Tests.ProtocolHandler.HTTP
             var mockSocket = new Mock<IChorizoSocket>();
             var byteCount = 0;
             mockSocket.Setup(sock => sock.Receive(It.IsAny<int>()))
-                .Returns(() => new Tuple<byte[], int>(new[] {testRequestBytes[byteCount++]}, 1));
+                .Returns(() => new Tuple<byte[], int>(new[] {_testGetRequestBytes[byteCount++]}, 1));
 
             var testResponse = new Response(mockSocket.Object);
 
@@ -64,11 +68,31 @@ namespace Chorizo.Tests.ProtocolHandler.HTTP
             testHandler.HandleRequest(mockSocket.Object);
 
             mockSocket.Verify(sock => sock.Receive(1), Times.Exactly(197));
-            mockRequestParser.Verify(rp => rp.Parse(testRequestString));
+            mockRequestParser.Verify(rp => rp.Parse(_testGetRequestString));
             mockRouter.Verify(route => route.Match(
                 It.Is<Request>(req => req.Equals(testRequest)),
                 It.Is<Response>(res => res.Equals(testResponse))
             ));
+        }
+
+        [Fact]
+        public void
+            HandleRequestThrowsNotImplementedErrorIfItRecievesEightThousandBytesBeforeItReceivesTwoConsecutiveCRLFs()
+        {
+            var mockSocket = new Mock<IChorizoSocket>();
+
+            var fByte = Encoding.UTF8.GetBytes("F");
+            mockSocket.Setup(sock => sock.Receive(It.IsAny<int>())).Returns(new Tuple<byte[], int>(fByte, 1));
+
+            var mockRequestParser = new Mock<IRequestParser>();
+            var mockRouter = new Mock<IRouter>();
+
+            var testHandler = new ChorizoHTTPConnectionHandler(
+                rawRequestParser: mockRequestParser.Object,
+                router: mockRouter.Object
+            );
+
+            Assert.Throws<NotImplementedException>(() => { testHandler.HandleRequest(mockSocket.Object); });
         }
     }
 }
