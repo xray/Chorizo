56debe84377183d815f261c964b3f598e73f267f
xray <creator@darkmattercreations.com>
2019-02-22 09:32:08 -0600

    Created and Passed: ParseTakesInBytesOfARequestWithHeadersAndReturnsARequest

--- tests/ProtocolHandler/DataParser/BasicDataParserTest.cs
+++ tests/ProtocolHandler/DataParser/BasicDataParserTest.cs
@@ -27,5 +27,30 @@ namespace Chorizo.Tests.ProtocolHandler.DataParser
 
             Assert.True(result.Equals(testRequest));
         }
+
+        [Fact]
+        public void ParseTakesInBytesOfARequestWithHeadersAndReturnsARequest()
+        {
+            const string testGetRequestString = "GET / HTTP/1.1\r\n" +
+                                                "foo: bar\r\n" +
+                                                "\r\n";
+            var testGetRequestBytes = Encoding.UTF8.GetBytes(testGetRequestString);
+
+            var testRequest = new Request(
+                "GET",
+                "/",
+                "HTTP/1.1",
+                new Dictionary<string, string>
+                {
+                    {"FOO", "bar"}
+                }
+            );
+
+            var testDataParser = new BasicDataParser();
+            var result = testDataParser.Parse(testGetRequestBytes);
+
+
+            Assert.True(result.Equals(testRequest));
+        }
     }
 }
